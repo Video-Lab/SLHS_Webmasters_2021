f81ffddad9d5415ec60f3d10bd1657a7914199ad
Video-Lab <jad@videolab.ae>
2021-02-19 16:27:44 -0600

    TSA page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+
+
 $(window).scroll(() => {
-    if($(this).scrollTop() > $(".hero").outerHeight()/4) {
+    if($(this).scrollTop() > $(".hero").outerHeight()/4 || $(this).outerWidth() <= 992) {
         $(".navbar").css("background", "rgb(17,17,64)");
     } else {
         $(".navbar").css("background", "rgba(0,0,0,0)");
