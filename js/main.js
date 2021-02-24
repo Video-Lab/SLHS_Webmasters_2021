5287c0b66f7e336d2195ee3bb2f02a7f30668b5f
Video-Lab <jad@videolab.ae>
2021-02-24 12:01:14 -0600

    Basic svg scroll

--- js/main.js
+++ js/main.js
@@ -12,15 +12,44 @@ function checkNavbarColor() {
 
 function dividerWidthScroll(threshold, offset) {
     $(".divider").each(function(){
-        var p = Math.max(0, Math.min(1, 1-($(".divider").offset().top+offset-($(window).height()+$(window).scrollTop()))/threshold));
+        var p = Math.max(0, Math.min(1, 1-($(this).offset().top+offset-($(window).height()+$(window).scrollTop()))/threshold));
         $(this).css("transform", "scaleX(" + p + ")");
         
     });
 
 }
 
+function getSVGItems($svg) {
+    return [...$svg.find("path"), ...$svg.find("circle")]
+}
+function setSVGDrawArray() {
+    $("svg").each(function(){
+        var items = getSVGItems($(this));
+        for(var i = 0; i < items.length; i++) {
+            var len = items[i].getTotalLength();
+            items[i].style['stroke-dasharray'] = len;
+            if(items[i].style['stroke'] === "") items[i].style['stroke'] = "rgb(49,113,207)";
+            if(items[i].style['stroke-width'] === "") items[i].style['stroke-width'] = "5px";
+            if(items[i].style['fill'] === "") items[i].style['fill'] = 'rgba(0,0,0,0)';
+        }
+    })   
+}
+
+function drawSVG(threshold, offset) {
+    $("svg").each(function(){
+        var items = getSVGItems($(this));
+        for(var i = 0; i < items.length; i++) {
+            var len = items[i].getTotalLength();
+            var p = Math.max(0, Math.min(1, ($(this).offset().top+offset-($(window).height()+$(window).scrollTop()))/threshold));
+            items[i].style['stroke-dashoffset'] = len*p;
+        }
+    })
+}
+
+setSVGDrawArray();
 $(window).scroll(() => {
     checkNavbarColor();
     dividerWidthScroll(threshold, offset);
+    drawSVG(threshold, offset);
 });
 $(window).resize(checkNavbarColor);
