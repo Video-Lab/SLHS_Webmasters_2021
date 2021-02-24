d9432a99937da56b1ad9fb0a37e0ffa16e635ce4
Video-Lab <jad@videolab.ae>
2021-02-24 01:37:51 -0600

    Navbar changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-$(window).scroll(() => {
-    if($(this).scrollTop() > $(".hero").outerHeight()/4 || $(this).outerWidth() <= 992) {
+function checkNavbarColor() {
+    if($(window).scrollTop() > 0 || $(window).outerWidth() <= 992) {
         $(".navbar").css("background", "rgb(17,17,64)");
+        $(".navbar").css("padding", "0.6em")
     } else {
         $(".navbar").css("background", "rgba(0,0,0,0)");
+        $(".navbar").css("padding", "1em 0.6em")
     }
-})
+}
+
+// $(document).ready(checkNavbarColor);
+$(window).scroll(checkNavbarColor);
+$(window).resize(checkNavbarColor);
