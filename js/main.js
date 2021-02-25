3fc46f322ab9382308b60c2a321921ee2c08e913
Video-Lab <jad@videolab.ae>
2021-02-25 08:26:54 -0600

    Navbar shadow

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ var baseAnimationTime = 1000;
 function checkNavbarColor() {
     if($(window).scrollTop() > 0 || $(window).outerWidth() <= 992) {
         $(".navbar").css("background", "rgb(17,17,64)");
+        $(".navbar").css("box-shadow", "0 2px 5px 1px rgba(0,0,0,0.5)")
         $(".navbar").css("padding", "0.6em")
     } else {
         $(".navbar").css("background", "rgba(0,0,0,0)");
+        $(".navbar").css("box-shadow", "none")
         $(".navbar").css("padding", "1em 0.6em")
     }
 }
@@ -73,6 +75,7 @@ function drawSVG() {
 
 
 setSVGDrawArray();
+checkNavbarColor();
 $(window).scroll(() => {
     checkNavbarColor();
     dividerWidthScroll();
--- js/tsa.js
+++ js/tsa.js
@@ -1,9 +1,11 @@
 function checkNavbarColor() {
     if($(window).scrollTop() > 0 || $(window).outerWidth() <= 992) {
         $(".navbar").css("background", "rgb(30,0,0)");
+        $(".navbar").css("box-shadow", "0 2px 5px 1px rgba(0,0,0,0.5)")
         $(".navbar").css("padding", "0.6em")
     } else {
         $(".navbar").css("background", "rgba(0,0,0,0)");
+        $(".navbar").css("box-shadow", "none")
         $(".navbar").css("padding", "1em 0.6em")
     }
 }
